c9729f7bd139b288c9df97c20f5ec5f030e2cbb2
Carol Alves <prof.carolalves90@gmail.com>
2022-06-18 10:50:02 -0300

    :wrench: configuração do app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const db = require("./database/mongooseConnect")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+db.connect()
+
+module.exports = app
