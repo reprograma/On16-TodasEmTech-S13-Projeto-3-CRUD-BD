1759cc1eacde979d9fd5d8e78608bb5101774efd
MajoBueno <buenomariajoao@gmail.com>
2022-06-18 10:20:14 -0300

    configurações iniciais

--- src/app.js
+++ src/app.js
@@ -2,3 +2,15 @@
 const express = require('express')
 // ativando app
 const app = express()
+
+// 1. body parse
+app.use(express.json())
+
+// 2. deixar a api publica
+app.use(cors())
+
+// 3. conectar o banco
+
+
+// exportar app
+module.exports = app
