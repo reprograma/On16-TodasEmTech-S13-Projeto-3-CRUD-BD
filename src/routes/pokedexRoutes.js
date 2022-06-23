99fa320cb5e52c994e3841eef6c47f07ce209f3c
PriscillaCorreia <primariafc@gmail.com>
2022-06-23 14:53:48 -0300

    atualiza rota de delete

--- src/routes/pokedexRoutes.js
+++ src/routes/pokedexRoutes.js
@@ -7,6 +7,7 @@ router.post("/pokedex", controller.createPokemon)
 router.get("/pokedex", controller.findAllPokemons)
 router.get("/pokedex/:id", controller.findPokemonById)
 router.patch("/pokedex/:id", controller.updatePokemonById)
-router.delete("/pokedex/:id", controller.deletePokemon)
+router.delete("/pokedex/:id", controller.deletePokemonById)
+
 
 module.exports = router
