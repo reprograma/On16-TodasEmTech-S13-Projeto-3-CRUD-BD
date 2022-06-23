2cda0cb60ec7cbb6a0218bd1926fe9d8a4e2a221
MajoBueno <buenomariajoao@gmail.com>
2022-06-22 22:22:42 -0300

    adicionada função de delete por id

--- src/controller/pokedexController.js
+++ src/controller/pokedexController.js
@@ -88,9 +88,31 @@ const updatePokemonById = async (req, res) => {
 
     }
 }
+
+
+const deletePokemonById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const findPokemon = await PokedexModel
+
+        if (findPokemon == null) {
+            return res.status(404).json({ message: `O pokémon com id# ${id}`})
+        }
+
+        await findPokemon.remove()
+
+        res.status(200).json({ message : `O pókemon ${findPokemon.name} foi deletado com sucesso` })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message})
+
+    }
+}
+
 module.exports = {
     createPokemon,
     findAllPokemons,
     findPokemonById,
-    updatePokemonById
+    updatePokemonById,
+    deletePokemonById
 }
