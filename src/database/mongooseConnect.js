0c48f4265aa8025c92a488f9e77905f0e529e0a2
PriscillaCorreia <primariafc@gmail.com>
2022-06-23 11:56:35 -0300

    atualiza o mongooseConnect

--- src/database/mongooseConnect.js
+++ src/database/mongooseConnect.js
@@ -1,12 +1,12 @@
-const  DATABASE_URI = "mongodb+srv://PriscillaFCorreia05:pridb0501@cluster0.yy3mi.mongodb.net/projeto_db"
+const DATABASE_URI = process.env.DATABASE_URI 
 
 const mongoose = require ('mongoose')
 const connect = async()=>{
 try{
-    await mongoose.connect(DATABASE_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+     mongoose.connect(DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
     console.log('banco conectado! ')
   } catch (error) {
