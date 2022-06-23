64bdb7e7bc7cae054f7e5e1fdd2cace4f9e7da85
nathalia <nacaldas@hotmail.com>
2022-06-23 15:53:24 -0300

    Cria porta

--- serve.js
+++ serve.js
@@ -1,3 +1,5 @@
-const app = require('./src/app.js')
+const app = require('./src/app')
 
-app.listen(8000, () => console.log(`fé no pai que agora vai: 8000`))
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`fé no pai que agora vai, porta: 8000`))
