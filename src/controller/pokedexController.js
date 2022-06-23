30f3658ee01e4ea8b8cb1f424aab9d7886438501
nathalia <nacaldas@hotmail.com>
2022-06-23 15:55:30 -0300

    Cria delete

--- src/controller/pokedexController.js
+++ src/controller/pokedexController.js
@@ -2,77 +2,71 @@ const PokedexModel = require('../models/pokedexModel')
 const CoachModel = require('../models/coachModel')
 
 const createPokemon = async (req, res) => {
-   try {
-     const { coachId, name, type, abilities, description } = req.body //  <-
+  try {
+    const { coachId, name, type, abilities, description } = req.body //  <-
 
-     if (!coachId) {
-       return res.status(400).json({ message: 'É obrigatorio o id do treinador'})
-      }
+    if (!coachId) {
+      return res.status(400).json({ message: 'É obrigatorio o id do treinador' })
+    }
 
-      const findCoach = await CoachModel.findById(coachId)
+    const findCoach = await CoachModel.findById(coachId)
 
-     if (!findCoach) {
-      return res.status(404).json({ message: 'Treinador não foi encontrado'})
-     }
+    if (!findCoach) {
+      return res.status(404).json({ message: 'Treinador não foi encontrado' })
+    }
 
-     // -->
-     const newPokemon = new PokedexModel({
+    // -->
+    const newPokemon = new PokedexModel({
       coach: coachId,
       name, type, abilities, description
-     })
+    })
 
-     const savedPokemon = await newPokemon.save()
+    const savedPokemon = await newPokemon.save()
 
-     res.status(200).json(savedPokemon)
+    res.status(200).json(savedPokemon)
 
-   } catch (error) {
+  } catch (error) {
     console.error(error)
     res.status(500).json({ message: error.message })
-   }
+  }
 }
 
 const findAllPokemons = async (req, res) => {
-   try {
-      const allPokemons = await PokedexModel.find().populate('coach')
-      res.status(200).json(allPokemons)
-   } catch (error) {
-    res.status(500).json({ message: error.message})
-   }
+  try {
+    const allPokemons = await PokedexModel.find().populate('coach')
+    res.status(200).json(allPokemons)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-const findPokemonById = async(req, res) => {
+const findPokemonById = async (req, res) => {
   try {
     const findPokemon = await PokedexModel
       .findById(req.params.id).populate('coach')
 
-     if (findPokemon == null) {
-      return res.status(404).json({ message: "pokemon não encontrado."})
-     }
+    if (findPokemon == null) {
+      return res.status(404).json({ message: "pokemon não encontrado." })
+    }
 
-      res.status(200).json(findPokemon)
+    res.status(200).json(findPokemon)
   } catch (error) {
-    res.status(500).json({ message: error.message})
+    res.status(500).json({ message: error.message })
   }
 }
 
-/**
- * 
- * 1. verificar se o pokemon existe [ x ]
- * 2. verificar se o coachId recebido existe
- * 3. verificar se o dado recebido é valido
- */
 const updatePokemonById = async (req, res) => {
   try {
     const { id } = req.params
     const { coachId, name, type, abilities, description } = req.body
     const findPokemon = await PokedexModel.findById(id)
     if (findPokemon == null) {
-      return res.status(404).json({ message: "pokemon não encontrado."})
+      return res.status(404).json({ message: "pokemon não encontrado." })
     }
     if (coachId) {
       const findCoach = await CoachModel.findById(coachId)
       if (findCoach == null) {
-        return res.status(404).json({ message: 'Treinador não foi encontrado'})
+        return res.status(404).json({ message: 'Treinador não foi encontrado' })
       }
     }
     // if (name) findPokemon.name = name
@@ -95,19 +89,36 @@ const deletePokemon = async (req, res) => {
     const { id } = req.params
     await PokedexModel.findByIdAndDelete(id)
     const message = `O pokemon com o ${id} foi deletado com sucesso!`
-   res.status(200).json({ message })
+    res.status(200).json({ message })
   } catch (error) {
     console.error(error)
-     res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
 }
 
+const deletePokemonById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const findPokemon = await PokedexModel.findById(id)
+
+    if (findPokemon == null) {
+      return res.status(404).json({ message: `O pokemon com o id# ${id}não foi encontrado.` })
+    }
+    await findPokemon.remove()
+
+    res.status(200).json({ message: `O pokemon ${findPokemon.name} foi deletado com sucesso.` })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
 
 module.exports = {
-   createPokemon, 
-   findAllPokemons, 
-   findPokemonById, 
-   updatePokemonById,
-   deletePokemon
+  createPokemon,
+  findAllPokemons,
+  findPokemonById,
+  updatePokemonById,
+  deletePokemon,
+  deletePokemonById
+
 
 }
