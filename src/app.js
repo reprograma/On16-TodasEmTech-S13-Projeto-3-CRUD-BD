cf8fbe50afabe7c80fadca2d58fff7176369bd58
Sarah Ribeiro <sm.lr@live.com>
2022-06-24 12:47:00 -0300

    dotenv add and connection upgrade

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('./database/mongooseConnect')
 
 app.use(express.json())
 app.use(cors())
+mongoose.connect()
 
-module.expors = app
+module.exports = app
