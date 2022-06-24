1a4a852f832b4c16b5ab0581a81c90a992daed8f
DayseDev <dayse_santos22@hotmail.com>
2022-06-24 19:48:45 -0300

    Projeto Guiado - Semana 13

---
+++ server.js
@@ -0,0 +1,7 @@
+// 1. importa o app(api)
+// 2. definir uma porta e ouvi-la
+const app = require('./src/app')
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`Fé no pai que agora vai, porta: 8000`))
