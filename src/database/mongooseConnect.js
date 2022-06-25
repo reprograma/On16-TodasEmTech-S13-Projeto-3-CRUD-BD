16a082f98e771f6be875e6dd8ab9eb1e72797b18
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-06-25 12:09:01 -0300

    Conectando banco de dados

---
+++ src/database/mongooseConnect.js
@@ -0,0 +1,19 @@
+// Conectando ao banco de dados
+
+const DATABASE_URI = process.env.DATABASE_URI;
+const mongoose = require('mongoose')
+const connect = async () => {
+    try {
+        await mongoose.connect(DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('Banco conectado')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = {
+    connect
+}
