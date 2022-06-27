c80bdcac7b51261a5a1191095638f381110a84da
Marina Porto <marinaporto334@gmail.com>
2022-06-26 23:46:29 -0300

    adicionei o app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const coachRoutes = require('./routes/coachRoutes')
+const pokedexRoutes = require('./routes/pokedexRoutes')
 
 const app = express()
 
@@ -14,6 +16,8 @@ app.use(cors())
 mongoose.connect()
 
 app.use(coachRoutes)
+app.use(pokedexRoutes)
+
 
 module.exports = app
 
