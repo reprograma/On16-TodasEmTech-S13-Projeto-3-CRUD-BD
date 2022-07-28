ffd16654e02621eb503e73ee6c154d4198893d28
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-07-28 02:14:48 -0300

    conexão bando de dados

--- src/database/mongooseConnect.js
+++ src/database/mongooseConnect.js
@@ -1,20 +1,18 @@
-const DATABASE_URI = process.env.DATABASE_URI
+const DATABASE_URI = "mongodb+srv://claudianefidelis:36ybplr5Xbf5Btln@cluster0.ijgvj.mongodb.net/Protagoniza-Pretx"
+const mongoose = require("mongoose")
 
-const mongoose = require('mongoose')
-
-const connect = async() => {
-   try {
-     await mongoose.connect(DATABASE_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-     })
-
-     console.log('banco conectado! ')
-   } catch (error) {
-    console.error(error)
-   }
+const connect = async () => {
+    try {
+        await mongoose.connect(DATABASE_URI, {
+            useNewUrlParser : true,
+            useUnifiedTopology : true 
+        })
+        console.log("Banco de dados conectado!")
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
-  connect
-}
+     connect 
+    }
