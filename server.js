9501a521cc359ea6949fbab851329d4ac3b2fd52
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-07-28 02:11:54 -0300

    add PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// 1. importa o app(api)
-// 2. definir uma porta e ouvi-la
 const app = require('./src/app')
+const PORT = 1313
 
-const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando, porta ${PORT}`)
+})
 
-app.listen(PORT, () => console.log(`fé no pai que agora vai, porta: 8000`))
